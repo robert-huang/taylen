295c67e9b38888957c8db536b9b808dd84754e70
Tristan Ohlson <tsohlson@gmail.com>
2020-01-09 00:17:05 -0500

    Better conclusion formatting

--- app/management/commands/conclude_emoji_matches.py
+++ app/management/commands/conclude_emoji_matches.py
@@ -7,8 +7,9 @@ from app.src.slack.slack_client import client
 class Command(BaseCommand):
     def handle(self, *args, **options):
         winning_emojis = []
+        losing_emojis = []
         ties = []
-        for match in EmojiMatch.objects.filter(winner=None, tied=False).all():
+        for match in EmojiMatch.objects.filter(winner=None, loser=None, tied=False).all():
             reactions = client.reactions_get(channel=match.slack_channel, timestamp=match.slack_ts)['message'][
                 'reactions']
             reaction_to_count = {}
@@ -22,19 +23,31 @@ class Command(BaseCommand):
 
             if first_votes > second_votes:
                 winning_emojis.append(match.first)
+                losing_emojis.append(match.second)
                 match.winner = match.first
                 match.loser = match.second
             elif first_votes < second_votes:
                 winning_emojis.append(match.second)
+                losing_emojis.append(match.first)
                 match.winner = match.second
                 match.loser = match.first
             else:
+                ties.append(match.first)
+                ties.append(match.second)
                 match.tied = True
-                ties.append(match)
 
             match.save()
 
         text = 'Winners: '
         for emoji in winning_emojis:
-            text += f':{emoji}: '
+            text += f':{emoji.name}: '
+
+        text += '\nLosers: '
+        for emoji in losing_emojis:
+            text += f':{emoji.name}: '
+
+        text += '\nTies: '
+        for emoji in ties:
+            text += f':{emoji.name}: '
+
         client.chat_postMessage(channel='#emoji-fight', text=text)
