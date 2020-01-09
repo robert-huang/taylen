0f26bbbf968edb95ba96728214cd9d21d44a4ddf
Tristan Ohlson <tsohlson@gmail.com>
2020-01-08 23:36:11 -0500

    Add a winners text when concluding matches

--- app/management/commands/conclude_emoji_matches.py
+++ app/management/commands/conclude_emoji_matches.py
@@ -6,6 +6,7 @@ from app.src.slack.slack_client import client
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
+        winning_emojis = []
         for match in EmojiMatch.objects.filter(winner=None).all():
             reactions = client.reactions_get(channel=match.slack_channel, timestamp=match.slack_ts)['message'][
                 'reactions']
@@ -20,4 +21,10 @@ class Command(BaseCommand):
                 match.winner = match.second
                 match.loser = match.first
 
+            winning_emojis.append(match.winner.name)
             match.save()
+
+        text = 'Winners: '
+        for emoji in winning_emojis:
+            text += f':{emoji}: '
+        client.chat_postMessage(channel='#emoji-fight', text=text)
